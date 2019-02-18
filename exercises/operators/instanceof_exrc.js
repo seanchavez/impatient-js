e5defc47193b92120f4e4f1a29c356220fcfc442
Sean Chavez <realseanchavez@gmail.com>
2019-02-17 23:12:36 -0800

    ch 11

--- exercises/operators/instanceof_exrc.js
+++ exercises/operators/instanceof_exrc.js
@@ -4,17 +4,17 @@ Instructions:
 - Change the second parameters of assert.equal() so that the test passes
 */
 
-import {strict as assert} from 'assert';
+import { strict as assert } from 'assert';
 
 test('instanceof', () => {
   //@ts-ignore
-  assert.equal('abc' instanceof String, '???');
+  assert.equal('abc' instanceof String, false);
   //@ts-ignore
-  assert.equal(null instanceof Object, '???');
+  assert.equal(null instanceof Object, false);
   //@ts-ignore
-  assert.equal(undefined instanceof Object, '???');
-  
-  assert.equal([true, false] instanceof Array, '???');
-  assert.equal([true, false] instanceof Object, '???');
-  assert.equal({} instanceof Object, '???');
+  assert.equal(undefined instanceof Object, false);
+
+  assert.equal([true, false] instanceof Array, true);
+  assert.equal([true, false] instanceof Object, true);
+  assert.equal({} instanceof Object, true);
 });
